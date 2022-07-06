c2fb32042a6bb4904ee8ffe18d8d4bda226f4a04
shj2022 <106890894+shj2022@users.noreply.github.com>
2022-07-06 15:04:57 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    logging = true
+})
+input.onButtonPressed(Button.B, function () {
+    logging = false
+})
+let logging = false
+logging = false
+loops.everyInterval(500, function () {
+    if (logging) {
+        datalogger.mirrorToSerial(true)
+        datalogger.log(datalogger.createCV("s", input.acceleration(Dimension.Strength)))
+    }
+})
+basic.forever(function () {
+	
+})
